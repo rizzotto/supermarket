743003718da6ad335616d93d16e6e59eedac3a38
Bruno Guerra <gguerrabruno@outlook.com>
2020-04-20 16:55:28 -0300

    Feat: installing router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,34 @@
 import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import ProductList from "./components/ProductList";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <ProductList />
+    <Router>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/caixa">Caixa</Link>
+          </li>
+          <li>
+            <Link to="/gerente">Gerente</Link>
+          </li>
+        </ul>
+      </nav>
+      <Switch>
+        <Route path="/caixa">
+          <ProductList />
+        </Route>
+      </Switch>
     </div>
+  </Router>
   );
 }
 
