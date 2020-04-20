72a340a38beae3e4467b4d5bbb1ab8fb0226d198
Bruno Guerra <gguerrabruno@outlook.com>
2020-04-20 13:49:25 -0300

    Open and close current day

--- server/src/routes.js
+++ server/src/routes.js
@@ -4,15 +4,35 @@ const routes = express.Router();
 const mongoose = require("mongoose");
 const Product = mongoose.model("Product");
 
-routes.get("/open", (req, res) => {
-  const date = new Date();
-  res.send({
-    date,
-  });
+let isOpen = false
+let curDay = new Date()
+
+/**
+ * Opens the current day as able to register sells
+ * @returns {Response}
+ */
+routes.post("/openDay", (req, res) => {
+  if(isOpen){
+    return res.sendStatus(400)
+  }
+  isOpen = true
+  return res.send('Day opened.')
+});
+
+/**
+ * Closes the current day, no more sells allowed.
+ * @returns {Response}
+ */
+routes.post("/closeDay", (req, res) => {
+  if(!isOpen){
+    return res.sendStatus(400)
+  }
+  isOpen = false
+  //Make report of sales
+  return res.send('Day closed.')
 });
 
 routes.post("/registerSell", (req, res) => {
-  const date = new Date();
   // res.send({
   //   date,
   // });
